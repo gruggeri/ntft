da1573fa633cace02cb52c5586c2bb9f4f19bec4
Xavier <xavier.adam@pm.me>
2019-11-17 16:46:54 +0100

    WIP

--- R/write.R
+++ R/write.R
@@ -1,3 +1,72 @@
+assert_valid_matches_sequence <- function(tokens_matches) {
+  if (!identical(unique(rle(tokens_matches)$lengths), 2L)) {
+    stop("It looks like you have some unclosed",
+         " or nested markdown tokens in a line.",
+         " See matches below:\n",
+         paste(tokens_matches, collapse = ", "))
+  }
+  tokens_matches
+}
+
+check_token_syntax <- function(txt_line_md, patterns) {
+  rgxs_to_search <- purrr::map_chr(patterns, "rgx")
+  grouped_rgx <- stringi::stri_c("(",rgxs_to_search,")") %>%
+    stringi::stri_c(collapse = "|")
+
+  tokens_matrix <- stringi::stri_match_all_regex(txt_line_md, grouped_rgx)[[1]]
+  tokens_matches <- tokens_matrix[,1]
+  if (all(is.na(tokens_matches))) {
+    return(character(0))
+  }
+  assert_valid_matches_sequence(tokens_matches)
+
+  last_non_na_col_per_row <- apply(tokens_matrix, 1, function(x) max(which(!is.na(x))))
+  last_non_na_col_per_row_dedup <- last_non_na_col_per_row[seq(1, length(last_non_na_col_per_row), 2)]
+  #positions_of_matches <- stringi::stri_locate_all_regex(txt_line_md, grouped_rgx)[[1]]
+  content_of_matches <- stringi::stri_split_regex(txt_line_md, grouped_rgx)[[1]]
+
+  non_plain <- data.frame(
+    name = names(rgxs_to_search)[last_non_na_col_per_row_dedup - 1],
+    #rgx = rgxs_to_search[last_non_na_col_per_row - 1],
+    #matches = tokens_matches,
+    stringsAsFactors = FALSE
+  )
+
+  plain <- data.frame(
+    name = rep("plain", nrow(non_plain) + 1),
+    stringsAsFactors = FALSE
+  )
+
+  dplyr::bind_rows(
+    dplyr::mutate(plain, row_n = dplyr::row_number()),
+    dplyr::mutate(non_plain, row_n = dplyr::row_number()),
+    .id = "src_id"
+  ) %>%
+    dplyr::arrange(row_n, src_id) %>%
+    dplyr::bind_cols(content = content_of_matches) %>%
+    dplyr::select(-src_id, -row_n)
+}
+
+sym_to_fun <- function(txt_line_md, patterns) {
+  token_to_search <- check_token_syntax(txt_line_md, patterns)
+
+  txt_line_fun <- paste0("plain(", txt_line_md, ")")
+  if (length(token_to_search) == 0)
+    return(txt_line_fun)
+
+  txt_line_fun <- list()
+  if(token_to_search$start[1] > 0) {
+    txt_line_fun <- stringi::stri_c(
+      "plain(", stringi::stri_split(txt_line_md, )
+    )
+  }
+
+  for(i in seq(1, 1:nrow(token_to_search), 2)) {
+
+  }
+}
+
+
 rep_sym_w_fun <- function(sentence, pattern) {
   md_positions <- str_locate_all(sentence, pattern$rgx)[[1]]
 
@@ -16,16 +85,30 @@ rep_sym_w_fun <- function(sentence, pattern) {
   sentence
 }
 
+
+
+text_colors <- list(
+  darkblue = list(red=6, green=58, blue=109),
+  medgrey = list(red=76, green=76, blue=76)
+)
+
+
+
+
+
 md_to_exp <- function(sentence) {
   md_bold <- list(md = "**",
                   rgx = "\\*\\*",
-                  fun = "bold")
+                  fun = "bold",
+                  color = list(red=76, green=76, blue=76))
   md_italic <- list(md = "*",
                     rgx = "\\*",
-                    fun = "italic")
+                    fun = "italic",
+                    color = list(red=76, green=76, blue=76))
   md_code <- list(md = "`",
                   rgx = "`",
-                  fun = "bolditalic")
+                  fun = "bolditalic",
+                  color = list(red=6, green=58, blue=109))
 
   sentence %>%
     rep_sym_w_fun(md_bold) %>%
