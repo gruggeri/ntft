bd064da31733530901621c32af157bb2e9ef5686
gruggeri <giulia.ruggeri@epfl.ch>
2020-04-24 12:22:29 +0200

    correct preview_quiz_list

--- R/preview_quiz_list.R
+++ R/preview_quiz_list.R
@@ -8,14 +8,14 @@
 #' @examples
 preview_quiz_list <- function(my_quiz) {
 
-  qz_list$answers %>%
+  my_quiz$answers %>%
     dplyr::mutate(text = forcats::fct_inorder(text) %>% forcats::fct_rev()) %>%
     ggplot2::ggplot(ggplot2::aes(x = 1, y = text)) +
     ggplot2::geom_label(ggplot2::aes(label = text, fill = is_correct), alpha = 0.6) +
     ggplot2::scale_fill_manual(values = c("red", "green"), guide = FALSE) +
     ggplot2::labs(
       title = "Quiz Preview",
-      subtitle = stringr::str_wrap(qz_list$question)
+      subtitle = stringr::str_wrap(my_quiz$question)
     ) +
     ggraph::theme_graph()
 }
